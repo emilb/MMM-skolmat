bd718a2eeb70dfb6af2d85ac8656aeb8b3d69080
Emil Breding <emil.breding@cygni.se>
2020-03-14 19:55:20 +0100

    Added not showing anything on even weeks

--- MMM-skolmat.js
+++ MMM-skolmat.js
@@ -7,6 +7,10 @@ Module.register("MMM-skolmat", {
 
 	// Override dom generator.
 	getDom: function() {
+		if (!this.shouldDisplay()) {
+			return;
+		}
+
 		var wrapper = document.createElement("div");
 		wrapper.innerHTML = 'Skolmat';
 		if (this.weekMenu) {
@@ -62,5 +66,31 @@ Module.register("MMM-skolmat", {
 	getStyles: function() {
 		return ["skolmat.css"];
 	},
+
+	shouldDisplay: function() {
+		var weekDay = new Date().getDay();
+
+		// Only show if saturday or sunday on even weeks
+		if (this.getWeek() % 2 == 0 && (weekDay == 6 || weekDay == 0))
+			return true;
+
+		if (this.getWeek() % 2 != 0 && (weekDay == 6 || weekDay == 0))
+			return false;
+
+		return this.getWeek() % 2 != 0;
+	},
+
+	getWeek: function() {
+		// Returns the ISO week of the date.
+		var date = new Date();
+		date.setHours(0, 0, 0, 0);
+		// Thursday in current week decides the year.
+		date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+		// January 4 is always in week 1.
+		var week1 = new Date(date.getFullYear(), 0, 4);
+		// Adjust to Thursday in week 1 and count number of weeks from date to week1.
+		return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+							- 3 + (week1.getDay() + 6) % 7) / 7);
+	}
 });
 
